bfd07e2e99498a89e525570a1f77ce57fcbdf7d0
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-19 22:58:34 +0530

    Automatic opening of whatsapp and sending photo is working , message isnt working

---
+++ Whatsapp_Messenger/main.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options = webdriver.ChromeOptions();
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome('C:/Users/91938/Desktop/chromedriver.exe',chrome_options=options)
+driver.get('https://web.whatsapp.com/')
+sleep(12)
+
+file_path = "C:/Users/91938/Desktop/Wallpapers/1.png"
+group_name = "Amma"
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(group_name))
+user.click()
+
+attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
+attachment_box.click()
+
+image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+image_box.send_keys(file_path)
+
+sleep(3)
+
+send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+send_button.click()
+
+sleep(3)
+text_box = driver.find_element_by_xpath('//div[@class="_2HE1Z _1hRBM"]')
+text_box.send_keys("Yo")
+
+test_button = driver.find_element_by_xpath('//button[@class="_2Ujuu"]')
+test_button.click()
+
+
