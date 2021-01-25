b4446fef29e785d3689864241aa07f47aea396b8
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-25 23:58:00 +0530

    Update a feature to send the message to multiple contacts

--- Whatsapp_Messenger/main.py
+++ Whatsapp_Messenger/main.py
@@ -5,9 +5,10 @@ import os
 
 # Initial Details
 file_dir = "C:/Users/91938/Desktop/GIT_HUB/Projects/Whatsapp_Messenger/Birthday/"
-contact_name = "Daddy"
-msg = open(file_dir + contact_name + "/txt.txt", "r").read()
-media_files = file_dir + contact_name + "/Media/"
+event = "Republic_Day"
+contact_names = open(file_dir + event + "/Contact.txt", "r").read().split("\n")
+msg = open(file_dir + event + "/txt.txt", "r").read()
+media_files = file_dir + event + "/Media/"
 
 # Text to Audio Converter
 language = 'en'
@@ -17,50 +18,57 @@ output.save(media_files + "0 Audio.mp3")
 # Code to Save Login Details using cookies
 options = webdriver.ChromeOptions();
 options.add_argument('--user-data-dir=./User_Data')
-driver = webdriver.Chrome('C:/Users/91938/Desktop/Whatsapp_Messenger/chromedriver.exe', chrome_options=options)
+driver = webdriver.Chrome('C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe', chrome_options=options)
 
 # Open Website
 driver.get('https://web.whatsapp.com/')
-sleep(10)
+sleep(20)
 
-# Code to Find Element in the Search box
-user = driver.find_element_by_xpath('//div[@class="_1awRl copyable-text selectable-text"]')
-user.send_keys(contact_name)
-user.click()
-sleep(15)
 
-# Code to Click the Open first Chat box in Search Results
-opening_chat = driver.find_element_by_xpath(
-    '//*[@id="pane-side"]/div[1]/div/div/div[15]/div')
-opening_chat.click()
-sleep(5)
+def whatsapp_automater(name):
+    # Code to Find Element in the Search box
+    user = driver.find_element_by_xpath('//div[@class="_1awRl copyable-text selectable-text"]')
+    user.send_keys(name)
+    user.click()
+    sleep(15)
 
-# Listing Files in the folder
-files = os.listdir(media_files)
+    # Code to Click the Open first Chat box in Search Results
+    opening_chat = driver.find_element_by_xpath(
+        '//*[@id="pane-side"]/div[1]/div/div/div[15]/div')
+    opening_chat.click()
+    sleep(5)
 
-for i in range(len(files)):
-    attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
-    attachment_box.click()
-    image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-    image_box.send_keys(media_files + files[i])
-    sleep(1)
+    # Listing Files in the folder
+    files = os.listdir(media_files)
 
-    # Code to Send Media Files
-    send_media_box = driver.find_element_by_xpath(
-        '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
-    send_media_box.click()
+    for i in range(len(files)):
+        attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
+        attachment_box.click()
+        image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+        image_box.send_keys(media_files + files[i])
+        sleep(1)
+
+        # Code to Send Media Files
+        send_media_box = driver.find_element_by_xpath(
+            '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
+        send_media_box.click()
+        sleep(2)
     sleep(2)
-sleep(2)
 
-# Code To send text message
-message_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div')
-sleep(2)
-message_box.send_keys(msg)
+    # Code To send text message
+    message_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div')
+    sleep(2)
+    message_box.send_keys(msg)
 
-# Code to CLick Send Button
-message_sender = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span')
-message_sender.click()
-sleep(3)
+    # Code to CLick Send Button
+    message_sender = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span')
+    message_sender.click()
+    sleep(3)
+
+
+# Code to Send To Multiple Contacts
+for num in range(len(contact_names)):
+    whatsapp_automater(contact_names[num])
 
 # Code to CLose Driver
 driver.close()
