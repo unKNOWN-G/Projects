83ee3d6fdd45c9eb35a7c45ee6ec6510e0b16c5e
Giri <karnatisaivenkatagiri@gmail.com>
2021-01-26 21:00:37 +0530

    Made the code modular and the present version has individual option to slect weather which of media, audio, text should be sent to the contact person

--- Whatsapp_Messenger/main.py
+++ Whatsapp_Messenger/main.py
@@ -4,30 +4,16 @@ from time import sleep
 import os
 import pandas as pd
 
-# Initial Details
-file_dir = "C:/Users/91938/Desktop/GIT_HUB/Projects/Whatsapp_Assistant/Event/"
-event = "Republic_Day"
-contact_names = pd.read_csv(file_dir + event + "/Contact.csv")
-msg = open(file_dir + event + "/txt.txt", "r").read()
-media_files = file_dir + event + "/Media/"
 
-# Text to Audio Converter
-language = 'en'
-output = gTTS(text=msg, lang=language, slow=False)
-output.save(file_dir + event + "/0 Audio.mp3")
-
-# Code to Save Login Details using cookies
-options = webdriver.ChromeOptions();
-options.add_argument('--user-data-dir=./User_Data')
-driver = webdriver.Chrome('C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe', chrome_options=options)
-
-# Open Website
-driver.get('https://web.whatsapp.com/')
-sleep(12)
+# Text to Audio Converter using gTTs
+def txt_to_audio_converter():
+    language = 'en'
+    output = gTTS(text=msg, lang=language, slow=False)
+    output.save(file_dir + event + "/0 Audio.mp3")
 
 
 # Function to Send Audio only When needed
-def txt_to_audio(value):
+def audio_sender(value):
     if value == 1:
         attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
         attachment_box.click()
@@ -56,8 +42,26 @@ def txt_sender(value):
         sleep(2)
 
 
-# Main Function
-def whatsapp_automater(name, audio_value, text_value):
+# Function to send "all media" only When needed
+def media_sender(files, value):
+    if value == 1:
+        for i in range(len(files)):
+            attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
+            attachment_box.click()
+            image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+            image_box.send_keys(media_files + files[i])
+            sleep(1)
+
+            # Code to Send Media Files
+            send_media_box = driver.find_element_by_xpath(
+                '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
+            send_media_box.click()
+            sleep(2)
+        sleep(2)
+
+
+# Function select the person/group name
+def chat_selector(person_name):
     # Opening New chat Option
     user = driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[2]/div/span')
     user.click()
@@ -66,7 +70,7 @@ def whatsapp_automater(name, audio_value, text_value):
     search_box = driver.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[1]/span/div/span/div/div['
                                               '1]/div/label/div/div[2]')
     search_box.click()
-    search_box.send_keys(name)
+    search_box.send_keys(person_name)
     sleep(6)
 
     # Code to Click the Open first Chat box in Search Results
@@ -75,33 +79,43 @@ def whatsapp_automater(name, audio_value, text_value):
     opening_chat.click()
     sleep(5)
 
+
+# Main Function
+def whatsapp_automater(name, audio_value, text_value, media_value):
+    chat_selector(name)
+
     # Listing Files in the folder
-    files = os.listdir(media_files)
+    file_data = os.listdir(media_files)
 
-    for i in range(len(files)):
-        attachment_box = driver.find_element_by_xpath('//div[@title="Attach"]')
-        attachment_box.click()
-        image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-        image_box.send_keys(media_files + files[i])
-        sleep(1)
-
-        # Code to Send Media Files
-        send_media_box = driver.find_element_by_xpath(
-            '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
-        send_media_box.click()
-        sleep(2)
-    sleep(2)
-
-    txt_to_audio(audio_value)
-
+    media_sender(file_data, media_value)
+    audio_sender(audio_value)
     txt_sender(text_value)
-
     sleep(2)
 
+
+# Initial Details
+file_dir = "C:/Users/91938/Desktop/GIT_HUB/Projects/Whatsapp_Messenger/Event/"
+event = "Republic_Day"
+contact_names = pd.read_csv(file_dir + event + "/Contact.csv")
+msg = open(file_dir + event + "/txt.txt", "r").read()
+media_files = file_dir + event + "/Media/"
+
+# Code to Save Login Details using cookies
+options = webdriver.ChromeOptions();
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome('C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe', options=options)
+
+# Open Website
+driver.get('https://web.whatsapp.com/')
+sleep(12)
+
+# Calling a function for text to audio conversion
+txt_to_audio_converter()
 
 # Code to Send To Multiple Contacts
 for num in range(len(contact_names["Contact_name"])):
-    whatsapp_automater(contact_names.iloc[num, 0], contact_names.iloc[num, 1], contact_names.iloc[num, 2])
+    whatsapp_automater(contact_names.iloc[num, 0], contact_names.iloc[num, 1], contact_names.iloc[num, 2],
+                       contact_names.iloc[num, 3])
 
-# Code to CLose Driver
+# Code to Close Driver
 driver.close()
