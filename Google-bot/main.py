6505cc6ae537f8e303cd90830c6195ce0b64fd9c
Giri <karnatisaivenkatagiri@gmail.com>
2021-03-17 02:41:56 +0530

    Code File

--- Google-bot/main.py
+++ Google-bot/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import selenium.webdriver as webdriver
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+search_word = "Hema"
+driver = webdriver.Chrome('C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe')
+driver.get('https://google.com')
+sleep(4)
+search_box = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input')
+search_box.click()
+search_box.send_keys(search_word)
+clicker = driver.find_elements_by_xpath("//html/body/div[1]/div[3]/form/div[2]/div[1]/div[2]/div[2]/div[2]/center/input[1]")
+clicker.click()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+links = driver.find_element_by_xpath('//div[@class="yuRUbf"]//a')
+for link in links:
+    print(link.get_attribute("href"))
