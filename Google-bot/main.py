8913fe2e7d11a581aaf43d8ac7794b92e2376bc5
Giri <karnatisaivenkatagiri@gmail.com>
2021-03-19 10:40:33 +0530

    Added proper code to the search google

--- Google-bot/main.py
+++ Google-bot/main.py
@@ -1,14 +1,21 @@
 import selenium.webdriver as webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
 
-search_word = "Hemangani"
+input_text = input("Enter Your Search Text : ")
 driver = webdriver.Chrome('C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe')
-driver.get("https://www.google.com/search?q="+search_word)
-sleep(2)
 
-links = driver.find_elements_by_xpath('//div[@class="yuRUbf"]//a')
-urls = []
-for i in range(min(len(links),5)):
-    urls.append(links[i].get_attribute("href"))
-print(urls)
-driver.close()
+driver.get("https://www.google.com/search?q=" + input_text)
+sleep(2)
+sub_results = driver.find_elements_by_xpath("//div[@class='g']")
+n1 = min(5, len(sub_results))
+google_search_results = []
+print("----------------------------------------------------------------")
+print('Top {1} Google Search Results for "{0}" : '.format(input_text, n1))
+for i in range(n1):
+    google_search_results.append(
+        sub_results[i].find_element_by_xpath('.//div[@class="yuRUbf"]/a').get_attribute("href"))
+    # driver.execute_script("window.open('{0}');".format(results[i]))
+    print(google_search_results[i])
+
