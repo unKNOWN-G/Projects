21856759441026b8a54768a73d9c5f619136ebc7
Giri <karnatisaivenkatagiri@gmail.com>
2021-03-28 13:27:59 +0530

    Added Code for Logging in into the Gmail Account

---
+++ Automatic_Email_Sender/main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from time import sleep
+
+# Gmail Login Credentials
+sender_email = "sherlocked@pragyan.org"
+sender_password = "sherlockphronesisdoctorstrange"
+
+# Gmail text
+receiver_email = "karnatisaievenkatagiri@gmail.com"
+subject = "Text"
+body = "We Share the same vibe boi"
+
+# Chrome Driver
+driverpath ='C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe'
+
+# Open Chrome
+driver = webdriver.Chrome(driverpath)
+driver.maximize_window()
+
+# Open Gmail Page
+driver.get("http://www.gmail.com")
+sleep(2)
+
+# Loginng in
+driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(sender_email)
+driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
+sleep(2)
+driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(sender_password)
+driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
+sleep(7)
+
+# Close Browser
+driver.close()
