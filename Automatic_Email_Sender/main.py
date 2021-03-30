95435c4082ed3fbc80c86e948806c947034aad7c
Giri <karnatisaivenkatagiri@gmail.com>
2021-03-30 18:12:56 +0530

    Added Few comments in main code

--- Automatic_Email_Sender/main.py
+++ Automatic_Email_Sender/main.py
@@ -5,6 +5,7 @@ import pandas
 contact_emails = pandas.read_csv("./Recipients_list.csv")
 Emails = contact_emails["Email"]
 
+# Send Emails
 mailer = EmailSender(sender_email=" ", sender_password=" ",
                      mail_subject=" ", mail_body=""" """)
 mailer.send_mails(driver_path="./chromedriver.exe", emails=Emails)
