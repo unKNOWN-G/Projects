a978d9898a55ceccb81a7c8fcb03db2d7c22a224
Giri <karnatisaivenkatagiri@gmail.com>
2021-03-30 08:43:36 +0530

    Added code to read a .csv file and send the mail to all recipients

--- Automatic_Email_Sender/main.py
+++ Automatic_Email_Sender/main.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 from time import sleep
+import pandas as pd
 
 # Gmail Login Credentials
-sender_email = " "
-sender_password = " "
+sender_email = "sender@gmail.com"
+sender_password = "sender_passoword"
 
 # Gmail text
-receiver_email = " "
-subject = " "
-body = " "
+subject = "Mail_subject"
+body = "Mail_body"
+
+# Read Emails
+contact_emails = pd.read_csv("./Recipients_list.csv")
+Emails = contact_emails["Email"]
+print(Emails)
 
 # Chrome Driver
 driverpath ='C:/Users/91938/Desktop/GIT_HUB/chromedriver.exe'
@@ -29,26 +34,28 @@ driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send
 driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
 sleep(7)
 
-# Compose
-try:
-    driver.find_element_by_css_selector('.z0>.L3').click()
-except:
-    driver.find_element_by_css_selector('.z0>.L3::before').click()
-sleep(1)
 
-# Input Recipient
-driver.find_element_by_css_selector(".oj div textarea").send_keys(receiver_email)
-sleep(0.5)
+for i in range(len(Emails)):
+    # Compose
+    try:
+        driver.find_element_by_css_selector('.z0>.L3').click()
+    except:
+        driver.find_element_by_css_selector('.z0>.L3::before').click()
+    sleep(1)
 
-# Input Subject
-driver.find_element_by_css_selector(".aoD.az6 input").send_keys(subject)
-sleep(0.5)
+    # Input Recipient
+    driver.find_element_by_css_selector(".oj div textarea").send_keys(Emails[i])
+    sleep(0.5)
 
-# Input Text
-driver.find_element_by_css_selector(".Ar.Au div").send_keys(body)
-sleep(0.5)
+    # Input Subject
+    driver.find_element_by_css_selector(".aoD.az6 input").send_keys(subject)
+    sleep(0.5)
 
-print("Email Sent to "+receiver_email)
+    # Input Text
+    driver.find_element_by_css_selector(".Ar.Au div").send_keys(body)
+    sleep(0.5)
+
+    print("Email Sent to " + Emails[i])
 
 # Close Browser
 driver.close()
